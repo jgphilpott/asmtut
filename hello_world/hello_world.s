c7abbe1ef1a078d595d60e6d9391a18e03d95065
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-13 17:19:59 -0500

    dividing tutorial sections

--- hello_world/hello_world.s
+++ hello_world/hello_world.s
@@ -3,17 +3,8 @@
 .global _start
 
 _start:
-  MOV R7, #4
-  MOV R0, #1
-  MOV R2, #12
-  LDR R1, =message
-  SWI 0
-
-end:
+  @ Some code here.
+  MOV R0, #65
   MOV R7, #1
-  SWI 0
-
-.data
-
-message:
-  .ascii "Hello World\n"
+  
+SWI 0
